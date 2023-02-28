c4dcaa8550066370a2114ebaff44d2f0979d8005
yemanekb <yemanekb@gmail.com>
2023-02-28 11:16:58 -0800

    regular expression

--- 0x06-regular_expressions/4-repetition_token_3.rb
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # This script accepts one argument and passes it to a regular expression,
 # matching method
-# The regular expression must match School
+# The regular expression must match the given cases
 
-puts ARGV[0].scan(/School/).join
-
+puts ARGV[0].scan(/hbt*n/).join
--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 # This script accepts one argument and passes it to a regular expression,
 # matching method
-# The regular expression must match School
+# The regular expression must match a 10 digit phone number
 
-puts ARGV[0].scan(/School/).join
-
+puts ARGV[0].scan(/^\d{10}$/).join
